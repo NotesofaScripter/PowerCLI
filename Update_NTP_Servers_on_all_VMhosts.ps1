07582955002c3dcecae260e575c1e33e09441759
NotesofaScripter <swyerdon@gmail.com>
2016-02-23 11:00:52 -0500

    Added files via upload

---
+++ Update_NTP_Servers_on_all_VMhosts.ps1
@@ -0,0 +1,16 @@
+﻿$oldntpservers='192.168.0.1','192.168.0.2'
+$newntpservers='0.vmware.pool.ntp.org','1.vmware.pool.ntp.org','2.vmware.pool.ntp.org'
+
+foreach($vmhost in get-vmhost){
+#stops ntpd service
+$vmhost|Get-VMHostService |?{$_.key -eq 'ntpd'}|Stop-VMHostService -Confirm:$false
+
+#remove existing ntpservers
+$vmhost|Remove-VMHostNtpServer -NtpServer $oldntpservers -Confirm:$false
+
+#adds new ntpservers
+$vmhost|Add-VmHostNtpServer -NtpServer $newntpservers
+
+#start ntpd service
+$vmhost|Get-VMHostService |?{$_.key -eq 'ntpd'}|Start-VMHostService
+}
