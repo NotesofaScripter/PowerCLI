f1ba83be95034445277e4d63fb24f4838318e71e
Stuart <NotesofaScripter@users.noreply.github.com>
2016-02-26 10:43:19 -0500

    Added files via upload

---
+++ Add_port_group_to_all_VMHosts_with_PowerCli.ps1
@@ -0,0 +1,13 @@
+﻿$NetworkInfo = Import-CSV C:\scripts\logs\NetworkInfo.csv
+$ClusterName = "VM_Cluster"
+$VMHosts = Get-Cluster $ClusterName | Get-VMHost
+Foreach ($network in $NetworkInfo){
+    $NewPortSwitch = $network.NewPortSwitch
+    $VLANID = $Network.VLANID
+    Foreach ($VMHost in $VMHosts){
+        IF (($VMHost | Get-VirtualPortGroup -name $NewPortSwitch -ErrorAction SilentlyContinue) -eq $null){
+            Write-host "Creating $NewPortSwitch on VMhost $VMHost" -ForegroundColor Yellow
+            $NEWPortGroup = $VMhost | Get-VirtualSwitch | Select -last 1 | New-VirtualPortGroup -Name $NewPortSwitch -VLanId $VLANID
+        }
+    }
+}
