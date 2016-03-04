620dc4b7f3a1370d92e76f7efe3c9709485e1a86
Stuart <NotesofaScripter@users.noreply.github.com>
2016-03-04 11:09:44 -0500

    Added files via upload

---
+++ How_to_get_the_free_space_on_all_datastores_on_a_Cluster_with_PowerCLI.ps1
@@ -0,0 +1,3 @@
+﻿function Get-FreeSpace ($clustername) {
+get-cluster $clustername | Get-Datastore | Where {$_.Name -notlike "*ISO*" -and $_.Name -notlike "*template*" } | sort -Property FreeSpaceGB -Descending
+}
