129363f24df4e6901e14f83a42b1d6803a6a00dc
Stuart <NotesofaScripter@users.noreply.github.com>
2016-03-14 09:40:34 -0400

    Added files via upload

---
+++ PowerCli_to_get_all_VMs_on_certain_Datastores.ps1
@@ -0,0 +1,9 @@
+﻿$report = @()
+
+$VMs = (Get-Datastore | Where {$_.Name -like '*REPL*' -or $_.Name -like '*PROD*'} | Get-VM).Name | sort
+
+Foreach ($VM in $VMs){
+    $line = Get-VM $VM | Select Name, @{N="vCPU";E={($_).NumCpu}}, @{N="Memory (GB)";E={($_).MemoryGB}}, @{N="Cluster";E={Get-Cluster -VM $_}}, @{N="Folder";E={$_.folder}}, @{N="Network";E={(Get-NetworkAdapter -VM $_).NetworkName}}, @{Expression={if (($_ | Get-HardDisk | Where {$_.DiskType -eq "RawPhysical"}) -eq $Null) {"No"} Else {"Yes"}}; Label="RDMs" }
+    $report += $line
+}
+$report | Export-Csv C:\scripts\logs\ProductionVMs.csv -NoTypeInformation -UseCulture
